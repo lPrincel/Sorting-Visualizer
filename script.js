673377e19652e45be47db2f6fe96b30a599146a6
lPrincel <princeprajesh2000@gmail.com>
2026-02-23 00:40:25 +0530

    Added selection and insertion sort

--- script.js
+++ script.js
@@ -40,4 +40,66 @@ async function bubbleSort(){
     }
     bars[0].style.backgroundColor="#2ecc71";
 }
+
+async function selectionSort(){
+    let bars=document.getElementsByClassName("bar");
+
+    for(let i=0;i<array.length;i++){
+        let index=i;
+        bars[index].style.backgroundColor="yellow";
+        for(let j=i+1;j<array.length;j++){
+            bars[j].style.backgroundColor="red";
+            await sleep(50);
+            if(array[j]<array[index]){
+                bars[index].style.backgroundColor="#3498db";
+                index=j;
+                bars[index].style.backgroundColor="yellow";
+            }
+            if(index!=j) bars[j].style.backgroundColor="#3498db";
+        }
+        if(index!=i){
+            let temp=array[i];
+            array[i]=array[index];
+            array[index]=temp;
+
+            bars[i].style.height=`${array[i]*3}px`;
+            bars[index].style.height=`${array[index]*3}px`;
+
+            bars[index].style.backgroundColor="#3498db";
+        }
+        bars[i].style.backgroundColor="#2ecc71";
+    }
+}
+
+async function insertionSort(){
+    let bars=document.getElementsByClassName("bar");
+    bars[0].style.backgroundColor = "#2ecc71";
+    
+    for(let i=1;i<array.length;i++){
+        let key=array[i];
+        let j=i-1;
+
+        bars[i].style.backgroundColor = "yellow";
+        await sleep(50);
+
+        while(j>=0 && array[j]>key){
+            bars[j].style.backgroundColor = "red";
+
+            array[j+1]=array[j];
+            bars[j + 1].style.height = `${array[j] * 3}px`;
+
+            await sleep(50);
+
+            bars[j].style.backgroundColor = "#2ecc71";
+            j--;
+        }
+
+        array[j+1]=key;
+        bars[j + 1].style.height = `${key * 3}px`;
+
+        bars[j+1].style.backgroundColor="#2ecc71";
+        
+        bars[i].style.backgroundColor = "#2ecc71";
+    }
+}
 generateArray();
